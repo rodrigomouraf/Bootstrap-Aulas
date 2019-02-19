99dd90c174977dec9e030958332b4cf847005a4f
Rodrigo Moura <rodrigomoura_rm@hotmail.com>
2019-02-19 17:35:38 -0300

    Aulas: 1 a 29

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+    // equivalente ao load da nossa pagina
+
+    $("#modal01").modal({backdrop:false, keyboard:false});    
+
+    $("#modal01").modal("show");
+
+    setTimeout(function(){
+        $("#modal01").modal("hide")
+    }, 3000);
+
+    $(".ttp").tooltip({
+        animation:false,
+        delay: {show: 1000, hide: 5000},
+        title: "Titulo Padrão",
+        //trigger: 'click'
+    });
+
+    $(".ppv").popover({
+       title: "Título do POPOVER",
+       trigger: 'hover focus' 
+    });
+
+    $(".btnjs").button();
+
+    $("#troca-estado").click(function(){
+        var btn = $(this);
+        btn.button("loading");
+
+        setTimeout(function(){
+            btn.button("reset");
+        }, 3000)
+    })
+});
